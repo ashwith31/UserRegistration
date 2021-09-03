3bcf1535860f777e05dbaa0539f67f51652cb321
ashwith31 <ashwitash@gmail.com>
2021-09-03 20:48:57 +0530

    modified:Password contains atleast one number

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -15,7 +15,7 @@ public class UserRegistration {
 	private static final String LAST_NAME_PATTERN = "^[A-Z][a-zA-Z]{3,}$";
 	private static final String EMAIL_ADDRESS = "^[a-z][a-zA-Z0-9]+([._+#-][a-zA-Z0-9]+)*@[a-zA-Z0-93]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
 	private static final String MOBILE_FORMAT = "^[1-9]{2}\\s[1-9][0-9]{9}$";
-	private static final String PASSWORD = "[A-Z]{1,}[a-z]{8,}$";
+	private static final String PASSWORD = "^(?=.*[A-z])(?=.*[0-9])(?=.*[a-z]).{8,}$";
 
 	/**
 	 * method for producing the Result in terms of Valid or Invalid
@@ -82,6 +82,7 @@ public class UserRegistration {
 	 * method for Validating Password.
 	 *  Rule1: minimum 8 Characters
 	 *  Rule2: Atleast 1 Uppercase Letter
+	 *  Rule3:Atleast 1 number.
 	 */
 	private void passwordValidation() {
 		System.out.println("Enter the Password");
